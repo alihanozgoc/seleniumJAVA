59217f29ff0b7743775264d408358317196f795c
Alihan <92430766+alihanozgoc@users.noreply.github.com>
2023-04-21 16:29:22 +0300

    Update MainPage.java

--- AmazonTests/src/test/java/pages/MainPage.java
+++ AmazonTests/src/test/java/pages/MainPage.java
@@ -3,12 +3,10 @@ package pages;
 import org.openqa.selenium.WebDriver;
 
 public class MainPage extends Page{
-
+    
     public final String myAccount = "nav-link-accountList";  // ID
-
     public final String search = "twotabsearchtextbox";  // ID
-
-
+    
     public MainPage(WebDriver driver) {
         super(driver);
     }
